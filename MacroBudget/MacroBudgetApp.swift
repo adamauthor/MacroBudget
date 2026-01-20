0741bf6e5cb3e0030eb31067503219f3c404affb
olzhasov_a <Olzhasov_A@bankrbk.kz>
2026-01-20 15:18:50 +0500

    Initial Commit

---
+++ MacroBudget/MacroBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacroBudgetApp.swift
+//  MacroBudget
+//
+//  Created by Akhmediyar Olzhassov on 20.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct MacroBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
